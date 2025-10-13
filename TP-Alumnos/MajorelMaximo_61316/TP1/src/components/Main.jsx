57961599614552ec3688dcd881a69fb28ec003f8
Maxi Majorel <maximomajorel@gmail.com>
2025-10-13 12:06:31 -0300

    Seccion soft skills

--- TP-Alumnos/MajorelMaximo_61316/TP1/src/components/Main.jsx
+++ TP-Alumnos/MajorelMaximo_61316/TP1/src/components/Main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Estudios } from "./Estudios";
+import { SoftSkills } from "./SoftSkills";
 
 export const Main = () => {
   return (
     <>
       <Estudios />
+      <SoftSkills />
     </>
   );
 };
