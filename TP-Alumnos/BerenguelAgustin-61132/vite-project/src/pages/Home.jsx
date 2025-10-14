1178e10f100db9cdbd8cf20c0e218e754e5c7759
Agustín <agustinberenguel1999@gmail.com>
2025-10-13 22:46:25 -0300

    Sumo seccion de estudios y proyectos

--- TP-Alumnos/BerenguelAgustin-61132/vite-project/src/pages/Home.jsx
+++ TP-Alumnos/BerenguelAgustin-61132/vite-project/src/pages/Home.jsx
@@ -1,13 +1,15 @@
-import React from "react";
-import Header from "../components/Header";
+import React from 'react';
+import Header from '../components/Header';
+import Estudios from '../components/Estudios';
+import Proyectos from '../components/Proyectos';
 
-
-const Home = () => 
-    {
-      return(
-      <>
+const Home = () => {
+  return (
+    <>
       <Header/>
-      </>
-      );
-    };
+      <Estudios/>
+      <Proyectos/>
+    </>
+  );
+};
 export default Home;
