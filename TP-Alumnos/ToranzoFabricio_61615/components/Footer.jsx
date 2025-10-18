9f34ca9e8944a008d011ac28f39e2c3c57d75a81
Fabricio Toranzo <fabriciotoranzodk@gmail.com>
2025-10-18 13:08:42 -0300

    feat: Footer modificado

--- TP-Alumnos/ToranzoFabricio_61615/components/Footer.jsx
+++ TP-Alumnos/ToranzoFabricio_61615/components/Footer.jsx
@@ -3,13 +3,14 @@ export default function Footer() {
     <footer className="footer">
       <p>© 2025 Fabricio Toranzo — Portfolio React</p>
       <nav>
-        <a href="#estudios">Estudios</a> |
-        <a href="#proyectos">Proyectos</a> |
-        <a href="#softskills">Soft Skills</a>
+        <a href="#estudios">Estudios</a>  | 
+        <a href="#proyectos">Proyectos</a>  |
+        <a href="#softskills">Habilidades</a>  |
+        <a href="#lenguajes">Lenguajes</a>  |
+        <a href="#idiomas">Idiomas</a>
       </nav>
       <div>
         <a href="https://github.com/fabricioo77" target="_blank">GitHub</a> |
-        <a href="https://linkedin.com" target="_blank">LinkedIn</a>
       </div>
     </footer>
   );
