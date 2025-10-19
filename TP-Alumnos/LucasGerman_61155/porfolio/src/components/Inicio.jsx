0f90c155c8674110db6a3d49fbd8e0e585074fe9
lucas <lucas00german@gmail.com>
2025-10-19 09:47:49 -0300

    agregue un h2 en inicio

--- TP-Alumnos/LucasGerman_61155/porfolio/src/components/Inicio.jsx
+++ TP-Alumnos/LucasGerman_61155/porfolio/src/components/Inicio.jsx
@@ -12,9 +12,11 @@ export default function Inicio({id}) {
                         
                    <h1 className="h1-inicio">Hola, soy <span className="nombre-color">Lucas</span> German Dávalos </h1>
 
+                   <h2 className="h2-inicio">Desarrollador Back-End</h2>
+
                     <p className="p-inicio">
-                        Soy una persona proactiva, con gran capacidad de aprndizaje, adaptacion y trabajo en equipo.
-                        Me apasiona la tecnología y disfruto desarrollar soluciones prácticas. Actualmente estudios
+                        Soy una persona proactiva, con gran capacidad de aprendizaje, adaptación y trabajo en equipo.
+                        Me apasiona la tecnología y disfruto desarrollar soluciones prácticas. Actualmente estudio
                         programación, lo que me permite aplicar mis conocimientos en proyectos reales.
                     </p>
                     </div>
