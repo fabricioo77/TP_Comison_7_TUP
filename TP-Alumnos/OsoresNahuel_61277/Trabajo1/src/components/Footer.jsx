4b5d5faeb76a508f213f5b9720a2cca1a9bb3c13
Shadow <nosores9@gmail.com>
2025-10-19 02:34:29 -0300

    footer con enlaces GitHub

--- TP-Alumnos/OsoresNahuel_61277/Trabajo1/src/components/Footer.jsx
+++ TP-Alumnos/OsoresNahuel_61277/Trabajo1/src/components/Footer.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Logo from "../assets/github.png"
+import { nombre } from './Header'
 
 const Footer = () => {
   return (
-    <div>
-        Enlaces
+    <div id="Footer">
+
+        <a href="https://github.com/SilverShadow571" target="_blank" rel="noopener noreferrer">
+        <img src= {Logo} alt="LogoGithub" id='imgLog'/>
+        </a>
+
+
+        <p><i>
+            © 2025 - Desarrollado por {nombre}
+        </i></p>
+
     </div>
   )
 }
--- TP-Alumnos/OsoresNahuel_61277/Trabajo1/src/components/Header.jsx
+++ TP-Alumnos/OsoresNahuel_61277/Trabajo1/src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import foto from '../assets/foto.jpeg'
 
+export const nombre = "Nahuel Osores"
+
 const Header = () => {
   return (
-    <div>
-    <header>
-        <img src={foto} alt="foto personal" id='imgPersonal'/>
-        <h1>Nahuel Osores</h1>
-    </header>
+    
+    <div id="Header">
+        <img src={foto} alt="foto personal" id="imgPersonal"/>
+        <h1>{nombre}</h1>
     </div>
   )
 }
