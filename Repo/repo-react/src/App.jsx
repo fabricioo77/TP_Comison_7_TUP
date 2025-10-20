9a5633d2f95ec2818a279d4c344727b4e1a9c14e
ChocobarMatias <111932821+ChocobarMatias@users.noreply.github.com>
2025-10-20 03:46:09 -0300

    repo de vide useState,useEffect, Modales,props, destructuring,react-bootstrap,fromularios

--- Repo/repo-react/src/App.jsx
+++ Repo/repo-react/src/App.jsx
@@ -4,26 +4,10 @@ import HomePage from "./pages/HomePage"
 
 function App() {
 
-  // const nombre = "Miguel"
-  // console.log(`Hola ${nombre}`)
-  //pasamos nombre por props a HomePage
-
-
-  // const persona = [{nombre: "Miguel", edad: 30, ciudad: "Madrid",
-  //   Estudios:[{titulo:"Ingenieria Informatica", año:2020},
-  //      {titulo:"Master en React", año:2021}]},{nombre: "Ana", edad: 25, ciudad: "Barcelona",
-  //   Estudios:[{titulo:"Ingenieria de Telecomunicaciones", año:2019},
-  //      {titulo:"Master en Vue", año:2020}]}]
-
-  //      console.log("Estudios de personas = ",persona[0].Estudios[1].titulo )
 
   return (
     <>
-    {/* {persona.map((a,i)=><li key={i}>{a.nombre} - {a.edad} - {a.ciudad}
-    <ul>
-      {a.Estudios.map((e,j)=><li key={j}>{e.titulo} - {e.año}</li>)}
-    </ul>
-    </li>)} */}
+ 
     <HomePage />
     </>
   )
